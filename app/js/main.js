2b0eee9822ceabb21cc001243adfa072224237fe
jrhoads <joseph_rhoads@brown.edu>
2014-06-19 13:24:18 -0400

    Added template directive for results

--- app/js/main.js
+++ app/js/main.js
@@ -4,3 +4,12 @@ app.controller("MainCtrl", function($scope, $location){
   $scope.params= $location.search();
 });
 
+  app.directive("resultDocument", function() {
+    return {
+      restrict: "E",
+      scope :{
+        doc : "=record", 
+      },
+      templateUrl:"app/view/my_result_document.html",
+    }
+  });
